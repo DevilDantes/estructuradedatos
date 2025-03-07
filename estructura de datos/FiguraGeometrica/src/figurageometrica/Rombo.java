167d79a97f6ce4395205ef7de6ee4c578823b0b7
DevilDantes <160239574+DevilDantes@users.noreply.github.com>
2025-03-06 19:03:11 -0500

    Update Rombo.java

--- estructura de datos/FiguraGeometrica/src/figurageometrica/Rombo.java
+++ estructura de datos/FiguraGeometrica/src/figurageometrica/Rombo.java
@@ -4,19 +4,16 @@ public class Rombo {
     private double diagonalMayor;
     private double diagonalMenor;
     
-    // Constructor por defecto
     public Rombo() {
         this.diagonalMayor = 0;
         this.diagonalMenor = 0;
     }
     
-    // Constructor con parametros
     public Rombo(double diagonalMayor, double diagonalMenor) {
         setDiagonalMayor(diagonalMayor);
         setDiagonalMenor(diagonalMenor);
     }
     
-    // Getters y setters
     public double getDiagonalMayor() {
         return diagonalMayor;
     }
@@ -39,22 +36,18 @@ public class Rombo {
             System.out.println("Error: La diagonal menor debe ser mayor a 0.");
     }
     
-    // Metodo para validar si es un rombo
     public boolean esRombo() {
         return diagonalMayor > 0 && diagonalMenor > 0;
     }
     
-    // Calcula el area del rombo
     public double area() {
         return (diagonalMayor * diagonalMenor) / 2;
     }
     
-    // Calcula el lado del rombo a partir de las diagonales
     public double lado() {
         return Math.sqrt(Math.pow(diagonalMayor / 2, 2) + Math.pow(diagonalMenor / 2, 2));
     }
     
-    // Calcula el perimetro del rombo
     public double perimetro() {
         return 4 * lado();
     }
